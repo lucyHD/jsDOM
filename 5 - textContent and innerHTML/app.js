2ee199c1c7f0b56d3b2724c849a2067598455c7d
lucyHD <lucyhelendonnelly@gmail.com>
2020-05-06 11:48:35 +0100

    file 5 update

---
+++ 5 - textContent and innerHTML/app.js
@@ -0,0 +1,26 @@
+////variables///
+
+const placeholder =document.getElementById("placeholder");
+
+const input = document.getElementById("input");
+
+const submit = document.getElementById("submit");
+
+const list = document.getElementById("list");
+
+
+/////normal code///////
+
+
+// submit.addEventListener("click", () =>{
+//     placeholder.style.color = "goldenrod";
+//     placeholder.textContent = input.value;
+// })
+
+
+submit.addEventListener("click", () =>{    
+    
+    placeholder.style.color = "goldenrod";
+    placeholder.textContent = `<li>${input.value}</li>`;
+    list.innerHTML = `<li>${input.value}</li>`
+ })
