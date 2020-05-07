b48c77ae059f576e8ffecb26246baf1144acb1d6
lucyHD <lucyhelendonnelly@gmail.com>
2020-05-07 10:58:09 +0100

    file 10 update

---
+++ 10 - setTimeout/app.js
@@ -0,0 +1,6 @@
+window.setTimeout((something)=> {
+    console.log(something);
+}, 5000, "Greetings Everyone");
+
+
+//remember the syntax for .setTimeout scope.setTimeOut(function, [time delay, paramenter])
